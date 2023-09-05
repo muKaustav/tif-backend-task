c3093bd889ae04890502796632a2b6d654a38b1d
muKaustav <mu.kaustav@gmail.com>
2023-09-05 21:42:59 +0530

    add member/community/role routes, fix bugs

--- passport.js
+++ passport.js
@@ -2,8 +2,6 @@ require('dotenv').config()
 const bcrypt = require('bcrypt')
 const passport = require('passport')
 const Validator = require('validatorjs')
-const JWTstrategy = require('passport-jwt').Strategy
-const ExtractJWT = require('passport-jwt').ExtractJwt
 const localStrategy = require('passport-local').Strategy
 const User = require('./models').User
 
@@ -62,16 +60,5 @@ passport.use('signup', new localStrategy({
             })
     } catch (error) {
         return done(error)
-    }
-}))
-
-passport.use(new JWTstrategy({
-    secretOrKey: process.env.JWT_SECRET,
-    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-}, async (token, done) => {
-    try {
-        return done(null, token.user)
-    } catch (error) {
-        done(error)
     }
 }))
